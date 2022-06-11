27e5bd51076922bd554b38da99ab28db56dc4988
SD-github21 <sapandonde.gmail.com>
2022-06-11 09:51:32 -0500

    added validation rules for specific fields

--- models/Product.js
+++ models/Product.js
@@ -22,13 +22,19 @@ Product.init(
     },
     price: {
       type: DataTypes.DECIMAL(5,2),
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
 
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 10
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
